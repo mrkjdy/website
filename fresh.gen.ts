aec68a3cc257bda3b8e6e6cbbef8efdd43fce333
Mark Judy <32761859+mrkjdy@users.noreply.github.com>
2022-10-01 18:41:34 +0000

    fix: Update manifest after replacing _middleware.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,7 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_500.tsx";
-import * as $2 from "./routes/__middleware.ts";
+import * as $2 from "./routes/_middleware.ts";
 import * as $3 from "./routes/about.tsx";
 import * as $4 from "./routes/index.tsx";
 import * as $$0 from "./islands/App.tsx";
@@ -15,7 +15,7 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_500.tsx": $1,
-    "./routes/__middleware.ts": $2,
+    "./routes/_middleware.ts": $2,
     "./routes/about.tsx": $3,
     "./routes/index.tsx": $4,
   },
