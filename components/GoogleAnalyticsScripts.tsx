08bbefb39b0f8d8f911aabf38cfe178edb750912
Mark Judy <32761859+mrkjdy@users.noreply.github.com>
2023-07-19 17:51:57 +0000

    fix: Switch from srcdoc for GA script to dangerouslySetInnerHTML

--- components/GoogleAnalyticsScripts.tsx
+++ components/GoogleAnalyticsScripts.tsx
@@ -14,12 +14,14 @@ export default () => (
         >
         </script>
         <script
-          srcDoc={`
-          window.dataLayer = window.dataLayer || [];
-          function gtag() { dataLayer.push(arguments); }
-          gtag('js', new Date());
-          gtag('config', '${GA_TRACKING_ID}');
-        `}
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `,
+          }}
         >
         </script>
       </>
