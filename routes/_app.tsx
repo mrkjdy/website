aef057d34601c0e4fcb49535904823189bc85fb0
Mark Judy <32761859+mrkjdy@users.noreply.github.com>
2023-11-19 19:45:56 +0000

    feat: use Partial on main content

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,4 @@
-import { asset, Head } from "$fresh/runtime.ts";
+import { asset, Head, Partial } from "$fresh/runtime.ts";
 import Nav from "../islands/Nav.tsx";
 import { AppProps } from "$fresh/server.ts";
 import GoogleAnalyticsScripts from "../components/GoogleAnalyticsScripts.tsx";
@@ -17,8 +17,10 @@ export default ({ Component }: AppProps) => (
     </Head>
     <div class="flex flex-col items-center relative min-h-screen top-0">
       <Nav />
-      <main class="flex flex-col items-center mt-10 mb-20 w-full">
-        <Component />
+      <main class="flex flex-col items-center mt-10 mb-20 w-full" f-client-nav>
+        <Partial name="main">
+          <Component />
+        </Partial>
       </main>
       <footer class="w-full flex flex-row justify-center h-10 absolute bottom-0 text-gray-400">
         <p>
