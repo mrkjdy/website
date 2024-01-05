4ca011e3a53f4c222c94de4a531c9306659c4eb1
Mark Judy <32761859+mrkjdy@users.noreply.github.com>
2024-01-05 03:58:48 +0000

    refactor: use head instead of Head in _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,13 @@
-import { asset, Head, Partial } from "$fresh/runtime.ts";
+import { asset, Partial } from "$fresh/runtime.ts";
 import Nav from "../islands/Nav.tsx";
 import { PageProps } from "$fresh/server.ts";
 import GoogleAnalyticsScripts from "../components/GoogleAnalyticsScripts.tsx";
 
 export default ({ Component }: PageProps) => (
   <html>
-    <Head>
-      <GoogleAnalyticsScripts />
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title key="title">mrkjdy.dev</title>
       <link
         rel="icon"
@@ -14,8 +15,8 @@ export default ({ Component }: PageProps) => (
         href={asset("/images/favicon.png")}
       />
       <link rel="stylesheet" href={asset("/styles.css")} />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
+      <GoogleAnalyticsScripts />
+    </head>
     <body class="text-black dark:text-white font-sans bg-white dark:bg-[#0d1117]">
       <div class="flex flex-col items-center relative min-h-screen top-0">
         <Nav />
