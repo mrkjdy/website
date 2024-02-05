fc3f0f895104036da589a90411ff138e83896b2c
Mark Judy <32761859+mrkjdy@users.noreply.github.com>
2024-02-05 14:38:17 +0000

    chore: add a few more languages for prism syntax highlighting

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -5,5 +5,8 @@ import "prismjs/components/prism-rust.js?no-check";
 import "prismjs/components/prism-typescript.js?no-check";
 import "prismjs/components/prism-jsx.js?no-check";
 import "prismjs/components/prism-tsx.js?no-check";
+import "prismjs/components/prism-haskell.js?no-check";
+import "prismjs/components/prism-c.js?no-check";
+import "prismjs/components/prism-bash.js?no-check";
 
 export { createExtractor } from "$std/front_matter/mod.ts";
