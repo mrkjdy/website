352bfea5216a53d42c1801b98a2a6eda62ef269a
Mark Judy <32761859+mrkjdy@users.noreply.github.com>
2024-03-24 00:17:20 +0000

    feat: add sitemap plugin

--- fresh.config.ts
+++ fresh.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 import tailwindConfig from "./tailwind.config.ts";
 import { TailwindPluginOptions } from "$fresh/plugins/tailwind/types.ts";
+import { freshSEOPlugin } from "$fresh_seo";
+import manifest from "./fresh.gen.ts";
+import { postArray } from "./utils/posts.ts";
 
 export default defineConfig({
-  plugins: [tailwind(tailwindConfig as TailwindPluginOptions)],
+  plugins: [
+    tailwind(tailwindConfig as TailwindPluginOptions),
+    freshSEOPlugin(manifest, {
+      include: [...postArray.map(({ href }) => href)],
+    }),
+  ],
 });
